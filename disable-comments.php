4b6f82d357f451126bc2a16dfa3e048ac1ccf915
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2023-05-24 21:14:56 +0200

    v1.0.1

--- disable-comments.php
+++ disable-comments.php
@@ -22,7 +22,7 @@
  * @wordpress-plugin
  * Plugin Name: Inpsyde Disable Comments
  * Description: Entirely ditches comments as a WordPress feature.
- * Version: 1.0.0
+ * Version: 1.0.1
  * License: GPLv2+
  * Requires at least: 6.0
  * Requires PHP: 8.0
